90ddc4d3dab67102be58b929e3e84411f44a8f47
jitigamb2 <jitingambhir@live.com>
2016-02-02 22:05:26 -0700

    Week 3 - Day 3 Exercise Complete

--- callbackPractice.js
+++ callbackPractice.js
@@ -26,6 +26,10 @@ and what you should write is the sayHi function that makes the code above work,
 
   //Code Here for first
   
+function first(arr, cb) {
+	cb(arr[0]);
+}
+
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
@@ -41,6 +45,10 @@ first(names, function(firstName){
 
   //Code Here for last
 
+function last(arr, cb) {
+	cb(arr[arr.length-1]);
+}
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -52,12 +60,13 @@ last(names, function(lastName){
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
-
-
-
-
-
   //Code Here for multiply
+
+function multiply(arg1, arg2, cb) {
+	var result = arg1 * arg2;
+	return cb(result);
+}
+
 
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -74,6 +83,16 @@ multiply(4, 3, function(answer){
 
 
   //Code Here for contains
+function contains(arr, arg1, cb) {
+	var result = false;
+	
+	arr.forEach (function (val, index) {
+		if (arg1 === val) result = true;
+	});
+	return cb(result);
+}
+
+
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains(names, 'Colt', function(result){
@@ -95,14 +114,19 @@ contains(names, 'Colt', function(result){
 
     //Code Here for uniq
 
+function uniq(arr, cb) {
+	var resultArr = [];
+	arr.forEach(function(ele,index) {
+		if (resultArr.indexOf(ele) === -1) resultArr.push(ele);
+	});
+	return cb(resultArr);
+}
+
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
-
-
-
-
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
@@ -111,9 +135,15 @@ uniq(names, function(uniqArr){
 
     //Code Here for each
 
+function each(arr, cb) {
+	arr.forEach(function(ele, index) {
+		cb(ele, index);
+	}) ;
+}
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + ' position is ' + item)
+	console.log('The item in the ' + indice + ' position is ' + item);
 });
 
 
@@ -127,6 +157,14 @@ each(names, function(item, indice){
 
 
  //code here for getUserById
+
+function getUserById(objArr, id, cb ) {
+	objArr.forEach( function(user, index) {
+		if (user.id === id) { 
+			return cb(user);
+		}
+	});
+}
 
 var users = [
   {
